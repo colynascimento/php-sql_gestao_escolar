9bfe4f11044dcf6f898892754eb7d9c57946e740
Bruno <brunolourenco.silva15@gmail.com>
2025-09-03 21:17:17 -0300

    Criação do header.php e footer.php

---
+++ inludes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Gestão Escolar</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+
+<body>
+    <header>
+        <h1>Gestão Escolar</h1>
+    </header>
+    <main>

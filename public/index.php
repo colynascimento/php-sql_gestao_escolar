addd43fbc0a4570df1fb22f9e796f1bf9f4472d8
colynascimento <colymsnascimento@gmail.com>
2025-09-04 21:22:37 -0300

    setando ids e criando pasta imgs para imagem de fundo

--- public/index.php
+++ public/index.php
@@ -6,20 +6,20 @@
         <title>Login | Gestão Escolar</title>
         <link rel="stylesheet" href="../css/style.css">
     </head>
-    <body>
+    <body id="pagina-login">
 
-        <form action="../inludes/validacao_login.php" method="POST">
-            <h1>Gestão Escolar</h1>
-            <p>Preencha suas credenciais de login para continuar</p>
+        <form id="form-login" action="../inludes/validacao_login.php" method="POST">
+            <h1 id="form-titulo-login" >Gestão Escolar</h1>
+            <p id="form-subtitulo-login">Preencha suas credenciais de login para continuar</p>
 
-            <label for="usuario-email">E-mail</label>
-            <input type="email" name="usuario-email" placeholder="email@gestaoescolar.com.br" required>
+            <label class="credenciais-login" for="usuario-email">E-mail</label>
+            <input  class="credenciais-login" type="email" name="usuario-email" placeholder="email@gestaoescolar.com.br" required>
 
-            <label for="usuario-senha">Senha</label>
-            <input type="password" name="usuario-senha" placeholder="senhadeacesso" required>
+            <label  class="credenciais-login" for="usuario-senha">Senha</label>
+            <input  class="credenciais-login" type="password" name="usuario-senha" placeholder="senhadeacesso" required>
 
-            <div id="btn-container">
-                <button type="submit">Entrar</button>
+            <div id="container-btn-login">
+                <button id="form-btn" type="submit">Entrar</button>
             </div>
         </form>
 

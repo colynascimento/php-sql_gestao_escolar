3b900a3a7ad0f5e574eeecde4fe84ac49ecd5b0b
colynascimento <colymsnascimento@gmail.com>
2025-09-09 21:42:35 -0300

    inicializando pagina inicial

---
+++ inludes/pagina-inicial.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+
+    <section>
+        <table>
+            <thead>
+                <th scope='col'></th>
+            </thead>
+        </table>
+    </section>
+
+<?php include 'footer.php'; ?>

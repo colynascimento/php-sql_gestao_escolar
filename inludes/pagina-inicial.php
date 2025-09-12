0a45eb859d510fbed0f082cb904b1471135be6b9
colynascimento <colymsnascimento@gmail.com>
2025-09-12 20:27:25 -0300

    estilização tables, e footer

--- inludes/pagina-inicial.php
+++ inludes/pagina-inicial.php
@@ -4,26 +4,28 @@
         <table>
             <thead>
                 <tr>
-                    <th scope='col' colspan='4'>Turma: </th> <!-- Preencher com nome da turma -->
+                    <th scope='col' colspan='2' class='cor-tabela1'>Turma: </th> <!-- Preencher com nome da turma -->
+                    <th scope='col' colspan='2' class='cor-tabela1'></th>
                 </tr>
                 <tr>
-                    <th scope='col' colspan='4'>Professor: </th> <!-- Preencher com nome do professor -->
+                    <th scope='col' colspan='2' class='cor-tabela1'>Professor: </th> <!-- Preencher com nome do professor -->
+                    <th scope='col' colspan='2' class='cor-tabela1'></th>
                 </tr>
                 <tr>
-                    <th scope='col' colspan='2'>Disciplina: </th> <!-- Preencher com nome da disciplina -->
-                    <th scope='col' colspan='2'>Código da Disciplina: </th> <!-- Preencher com código da disciplina -->
+                    <th scope='col' colspan='2' class='cor-tabela2'>Disciplina: </th> <!-- Preencher com nome da disciplina -->
+                    <th scope='col' colspan='2' class='cor-tabela2'>Código da Disciplina: </th> <!-- Preencher com código da disciplina -->
                 </tr>
                 <tr>
-                    <th scope='col' colspan='1'>Turno: </th>
-                    <th scope='col' colspan='1'></th> <!-- Preencher com turno -->
-                    <th scope='col' colspan='1'>Sala: </th>
-                    <th scope='col' colspan='1'></th> <!-- Preencher com número da sala -->
+                    <th scope='col' colspan='1' class='cor-tabela2'>Turno: </th>
+                    <th scope='col' colspan='1' class='cor-tabela2'></th> <!-- Preencher com turno -->
+                    <th scope='col' colspan='1' class='cor-tabela2'>Sala: </th>
+                    <th scope='col' colspan='1' class='cor-tabela2'></th> <!-- Preencher com número da sala -->
                 </tr>
                 <tr>
-                    <th scope='col' colspan='1'>Nome</th>
-                    <th scope='col' colspan='1'>Data de Nascimento</th>
-                    <th scope='col' colspan='1'>CPF</th>
-                    <th scope='col' colspan='1'>Ações</th>
+                    <th scope='col' colspan='1' class='cor-tabela3'>Nome</th>
+                    <th scope='col' colspan='1' class='cor-tabela3'>Data de Nascimento</th>
+                    <th scope='col' colspan='1' class='cor-tabela3'>CPF</th>
+                    <th scope='col' colspan='1' class='cor-tabela3'>Ações</th>
                 </tr>
             </thead>
             <tbody>

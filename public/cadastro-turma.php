22f77f59da145c83f7fe391004ac4ed310e0c73d
colynascimento <colymsnascimento@gmail.com>
2025-09-18 21:43:13 -0300

    estilização formulario cadastro professores parcial

--- public/cadastro-turma.php
+++ public/cadastro-turma.php
@@ -6,32 +6,41 @@ include('../api/turma/exibirTurma.php');
 // include('../api/turma/editarTurma.php');
 
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro-Turmas</title>
-    <link rel = "stylesheet" href = "../css/style.css">
-</head>
 <body>
-    <h1>Cadastro de Turmas </h1>
-    <form id="cadTurma" method = "POST" action = "">
-        <label>Numero da turma: </label>
-        <input type = "number" id = "num_turma" name = "num_turma" required>
-        <label>Nome da turma: </label>
-        <input type = "text" id = "nomeTurma" name = "nomeTurma" required>
-        <label>Turno: </label>
-        <select name = "turno" id = "turno" required>
-            <option value = ""disabled selected>Selecione</option>
-            <option value = "manha">Manhã</option>
-            <option value = "tarde">Tarde</option>
-            <option value = "noite">Noite</option>
-        </select>
-        <label>Sala:</label>
-        <input type = "text" id = "sala" name = "sala" required><br>
-        <button type="button" name="btnCadTurma" id="btnCadTurma" >Cadastrar</button>
+    <form id="formTurmas" method = "POST" action = "">
+        <fieldset>
+            <legend id="legendaFormTurmas">Cadastrar Turma</legend>
+
+            <div class="formBox">
+                <label>Numero da turma: </label>
+                <input type = "number" id = "num_turma" name = "num_turma" required>
+            </div>
+
+            <div class="formBox">
+                <label>Nome da turma: </label>
+                <input type = "text" id = "nomeTurma" name = "nomeTurma" required>
+            </div>
+        </fieldset>
+
+        <fieldset>
+            <div class="formBox">
+                <label>Turno: </label>
+                <select name = "turno" id = "turno" required>
+                    <option value = ""disabled selected>Selecione</option>
+                    <option value = "manha">Manhã</option>
+                    <option value = "tarde">Tarde</option>
+                    <option value = "noite">Noite</option>
+                </select>
+            </div>
+        
+
+            <div class="formBox">
+                <label>Sala:</label>
+                <input type = "text" id = "sala" name = "sala" required><br>
+            </div>
+        </fieldset>
+
+        <button type="button" name="btnCadTurma" id="btnCadTurma">Enviar</button>
         <!-- <button onclick="apagarTurma()">Apagar Turma</button> -->
         
         <div id = "msg"></div>
